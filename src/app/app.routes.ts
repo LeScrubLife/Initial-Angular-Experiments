e59211b8e13afbb5540fbdefe38515f544a3f9a6
TheScrubLife <coleethen7@gmail.com>
2026-08-13 08:21:43 +0900

    Initial page setups and linking

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: 'contacts', component: ContactsComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
